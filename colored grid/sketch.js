0efb94c7f3621a788919b31ae08754b85ccbdc29
Devodore <theodore.konikowski@eemi.com>
2019-12-02 12:26:23 +0100

    v2.0

--- colored grid/sketch.js
+++ colored grid/sketch.js
@@ -1,91 +1,99 @@
 let s;
-let webImage;
+let webImages;
 
 function preload() {
     webImages = [];
-    webImages.push(loadImage("https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"));
-    webImages.push(loadImage("https://images.unsplash.com/photo-1575111100086-59332d3e47bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"));
+    webImages.push(loadImage('https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e3917e828b7c2502b0041813eff1294&auto=format&fit=crop&w=634&q=80'));
+    webImages.push(loadImage('https://images.unsplash.com/photo-1575140685026-1908fb64d279?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'));
+    webImages.push(loadImage('https://images.unsplash.com/photo-1575121274665-9dd0af2b0eea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'));
+    webImages.push(loadImage('https://images.unsplash.com/photo-1575146272339-e53cd2fd34bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'));
 }
 
 function setup() {
     createCanvas(600, 600);
-    s = {}
+    s = {};
     s.bgColor = '#222222';
-    s.rowCount = 50;
-    s.columnCount = 15;
+    s.rowCount = 15;
+    s.columnCount = 8;
+    s.seed = floor(random(10000));
     s.rectWidth = 8;
     s.oscAmplitude = 20;
     s.oscFrequency = 0.1;
+    s.oscModFrequency = 0.1;
     s.activeImageIndex = 0;
-    background(s.bgColor);
+    s.maxRotation = 60;
     initColors();
-    let gui= new dat.GUI();
-    let columnController = gui.add(s, 'columnCount', 1 ,200).name('nombre de colonnes');
-    let rowController = gui.add(s, 'rowCount', 1 ,200).name('nombre de lignes');
-    let activeImageIndexController = gui.add(s, 'activeImageIndex', [...webImages.keys()]).name('Image numéro');
-
-    activeImageIndexController.onChange(value => {
-        s.activeImageIndex = value;
-        initColors();
-    })
-    columnController.onChange(value => {
-        s.columnCount = floor(value);
-        initColors();
-    });
-    rowController.onChange(value => {
+    background(s.bgColor);
+    let gui = new dat.GUI();
+    gui.add(s, 'rectWidth', 0, 100).name('largeur rectangle');
+    let rowCountController = gui.add(s, 'rowCount', 1, 100).name('nombre lignes');
+    let columnCountController = gui.add(s, 'columnCount', 1, 100).name('nombre colonnes');
+    rowCountController.onChange(value => {
         s.rowCount = floor(value);
         initColors();
     });
-    gui.add(s, 'rectWidth', 0, 100).name('largeur rectange');
-    gui.add(s, 'oscFrequency',0, 1).name('Fréquence mouvement');
-    gui.add(s, 'oscAmplitude',0, 100).name('Amplitude de mouvement');
-    //gui.add(s, 'rowCount',0, 100).name('nombre de lignes');
-    //gui.add(s, 'columnCount',0, 100).name('nombre de colonnes');
+    columnCountController.onChange(value => {
+        s.columnCount = floor(value);
+        initColors();
+    });
     //gui.add(this, 'initColors');
-    gui.add(s, 'bgColor').name('Couleur de fond');
+    gui.addColor(s, 'bgColor').name('Couleur de fond');
+    let activeImageIndexController = gui.add(s, 'activeImageIndex', [...webImages.keys()]).name('Image');
+    activeImageIndexController.onChange(value => {
+        s.activeImageIndex = value;
+        initColors();
+    });
+    gui.add(s, 'maxRotation', 0, 180);
 }
 
-function initColors(){
+function initColors() {
     s.rectColors = [];
     let webImage = webImages[s.activeImageIndex];
-    s.seed = floor(random(1000));
-    for (let i = 0; i < s.rowCount * s.columnCount; i++){
-        let x = random(0, webImage.width)
-        let y = random(0, webImage.height)
+    for (let i = 0; i < s.rowCount * s.columnCount; i++) {
+        let x = random(0, webImage.width);
+        let y = random(0, webImage.height);
         let c = webImage.get(x, y);
         s.rectColors.push(c);
     }
-    background(s.bgColor);
 }
 
 function draw() {
     background(s.bgColor);
-    randomSeed(s.seed);
-    drawGrid(500, 500);
+    drawGrid(400, 400);
 }
 
 function drawGrid(w, h) {
     let cellWidth = w / s.columnCount;
     let cellHeight = h / s.rowCount;
     push();
-    translate((width -w)/2, (height - h)/2);
+    translate((width - w)/2, (height - h)/2);
     let k = 0;
-    for (let i = 0; i < s.columnCount; i++){
-        for (let j = 0; j < s.rowCount; j++){
+    for (let i = 0; i < s.columnCount; i++) {
+        for (let j = 0; j < s.rowCount; j++) {
             let c = s.rectColors[k];
             noStroke();
             fill(c);
             push();
-            let shift = s.oscAmplitude * sin(frameCount * s.oscFrequency+j);
-            shift+= s.oscAmplitude * sin(frameCount * (s.oscFrequency +j / s.rowCount * s.oscFrequency));
-            shift+= s.oscAmplitude * sin(frameCount * (s.oscFrequency +j / s.columnCount * s.oscFrequency));
-            translate((cellWidth - s.rectWidth) / 2 + shift, 0);
-            rect(i * cellWidth, j* cellHeight, s.rectWidth, cellHeight);
+            let shift = s.oscAmplitude * sin(frameCount * s.oscFrequency);
+            shift += s.oscAmplitude * sin(frameCount * (s.oscFrequency + i / s.columnCount * s.oscModFrequency));
+            shift += s.oscAmplitude * sin(frameCount * (s.oscFrequency + j / s.rowCount * s.oscModFrequency));
+            translate(i * cellWidth + cellWidth / 2 + shift, j * cellHeight + cellHeight / 2); // centering
+            rotate(j / s.rowCount * s.maxRotation * PI/180);
+            rectByCenter(0, 0, s.rectWidth, cellHeight);
+            // rectByCenter(i * cellWidth, j * cellHeight, s.rectWidth, cellHeight);
             pop();
             k++;
         }
     }
-    pop()
+    pop();
+}
+
+function rectByCenter(x, y, w, h) {
+    push();
+    translate(-w/2, -h/2);
+    rect(x, y, w, h);
+    pop();
+
 }
 
 function keyTyped() {
